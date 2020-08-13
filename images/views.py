da912a7344bfb731cf034814fed34460efb5f402
BlackSpartan <shokomelu@gmail.com>
2020-08-13 15:37:28 +0200

    Adding a view function for likes

--- images/views.py
+++ images/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .forms import ImageCreateForm
 from django.shortcuts import get_object_or_404
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 @login_required # login_required decorator is used to prevent access for unauthenticated users
@@ -37,3 +39,21 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html',{'section': 'images', 'image': image})
+
+# views for users to like images
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(require.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'OK'})
+        except:
+            pass
+    return JsonResponse({'status':'error'})
