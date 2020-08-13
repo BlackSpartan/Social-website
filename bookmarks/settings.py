9cc6de15308b78d982eb6775838aa1469307ac88
BlackSpartan <shokomelu@gmail.com>
2020-08-13 15:28:44 +0200

    adding easy_thumbnails to the installed apps

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig', 
+    'easy_thumbnails',
     'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
